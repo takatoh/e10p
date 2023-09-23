abf44b5f112f7c7893690cf0893baaffdbb30d4b
takatoh <takatoh.m@gmail.com>
2023-09-23 18:58:25 +0900

    e10p/node.py(Tree.__str__()): Fix.

--- e10p/node.py
+++ e10p/node.py
@@ -8,11 +8,16 @@ class Tree():
         self.b = Leaf(b) if isinstance(b, int) else b
 
     def __str__(self):
-        s = str(self.a) + self.op + str(self.b)
-        if self.op in ['+', '-']:
-            return f'({s})'
-        else:
-            return s
+        a = str(self.a)
+        b = str(self.b)
+        if self.op in ['*', '/']:
+            if isinstance(self.a, Tree) and self.a.op in ['+', '-']:
+                a = f'({a})'
+            if isinstance(self.b, Tree) and self.b.op in ['+', '-']:
+                b = f'({b})'
+        elif self.op == '-' and isinstance(self.b, Tree) and self.b.op == '-':
+            b = f'({b})'
+        return a + self.op + b
 
     def value(self):
         if self.op == '+':
