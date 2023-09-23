3f88d94bec0cb4ca8e5cab420d15c9f7f344412d
takatoh <takatoh.m@gmail.com>
2023-09-23 13:08:34 +0900

    e10p/command.py: New.

---
+++ e10p/command.py
@@ -0,0 +1,26 @@
+import argparse
+from .solver import Solver
+
+
+def main():
+    args = parse_arguments()
+    solver = Solver(4, 10)
+    result = solver.solv(args.nums)
+    print(result)
+
+
+def parse_arguments():
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        'nums',
+        action='store',
+        nargs='+',
+        type=int,
+        help='specify numbers'
+    )
+    args = parser.parse_args()
+    return args
+
+
+
+main()
